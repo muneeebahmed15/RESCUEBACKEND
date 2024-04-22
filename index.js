28d8d0a2404973412bcefc174fcf94cf17a4d31e
muneeebahmed15 <muneebahme999@gmail.com>
2024-04-23 01:04:15 +0500

    l

--- index.js
+++ index.js
@@ -10,23 +10,15 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json())
 
-//  //  "http://localhost:5173", 
-// app.use(cors({ origin:"https://jobrescuefrontend.vercel.app" }))
-
 const corsOptions = {
   origin: 'https://jobrescuefrontend.vercel.app/', // Specify the allowed origin
   methods: ['GET', 'POST', 'PUT'], // Specify the allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
 };
- //"http://localhost:5173",
 app.use(cors(corsOptions));
 
 //optionsS
 app.options("*", cors());
-
-// router.get('/', (req, res) => {
-//   res.send('Welcome to the rescue endpoint!');
-// });
 
 app.use("/v1/rescue", require("./routes/userRoutes"));
 
