d83d0365bc5820f618e77598ad1937b87b6f1a28
muneeebahmed15 <muneebahme999@gmail.com>
2024-05-02 00:40:34 +0500

    xx

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.options('*', cors());
 
 // Enable CORS middleware
 app.use(cors({
-    origin: ['http://localhost:5173', "https://jobrescuefrontend.vercel.app/"],
+    origin: ['http://localhost:5173', "https://jobrescuefrontend.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
