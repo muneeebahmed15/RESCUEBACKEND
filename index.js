bd6bce66720e3d72c843a58750f31d334b0a3a5b
muneeebahmed15 <muneebahme999@gmail.com>
2024-05-02 01:07:09 +0500

    m

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// require('dotenv').config();
-// const connectDB = require("./db/db");
-
-// const path = require('path');
-
-
-// connectDB();
-
-// const port = process.env.PORT || 4000;
-
-// app.use(express.json())
-
-// app.use(cors({
-//     origin: 'https://jobrescuefrontend.vercel.app'
-// }));
-
-
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
-// app.use("/v1/rescue", require("./routes/userRoutes"));
-
-// app.listen(port, () => {
-//     console.log(`Server is running on Port ${port}`);
-// });
-
-
 const express = require("express");
 const cors = require("cors");
 const path = require('path');
@@ -61,10 +31,6 @@ app.use(express.json());
 // Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-// app.use('/', (req, res) => {
-//     res.send('Welcome to the rescue endpoint! from the indexxxxxx');
-//   });
 
 // Define routes
 app.use("/v1/rescue", userRoutes);
