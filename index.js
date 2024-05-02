2fccb8505b052009b2b442a221d3095d0b6b54c7
muneeebahmed15 <muneebahme999@gmail.com>
2024-05-02 22:50:57 +0500

    xx

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 const connectDB = require("./db/db");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 // Load environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Connect to the database
 connectDB();
@@ -16,26 +16,38 @@ connectDB();
 const port = process.env.PORT || 4000;
 
 // Handle preflight requests
-app.options('*', cors());
+app.options("*", cors());
 
 // Enable CORS middleware
-app.use(cors({
-    origin: ['http://localhost:5173', "https://jobrescuefrontend.vercel.app"],
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://jobrescuefrontend.vercel.app",
+    ],
     methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // Parse JSON requests
 app.use(express.json());
 
 // Serve static files from the 'uploads' directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
 
+app.get("/", function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send("myRequest");
+});
 
 // Define routes
 app.use("/v1/rescue", userRoutes);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on Port ${port}`);
+  console.log(`Server is running on Port ${port}`);
 });
