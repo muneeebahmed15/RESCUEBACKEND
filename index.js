e8acd81f46e6fcb0ef915eed83163407b42cd272
muneeebahmed15 <muneebahme999@gmail.com>
2024-05-02 23:02:46 +0500

    a

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const connectDB = require("./db/db");
 const userRoutes = require("./routes/userRoutes");
@@ -16,19 +16,34 @@ connectDB();
 const port = process.env.PORT || 4000;
 
 // Handle preflight requests
-app.options("*", cors());
+// app.options("*", cors());
 
 // Enable CORS middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://jobrescuefrontend.vercel.app",
-    ],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://jobrescuefrontend.vercel.app",
+//     ],
+//     methods: ["POST", "GET", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+function customCors(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  next();
+}
+
+app.use(customCors);
 
 // Parse JSON requests
 app.use(express.json());
