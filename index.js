18dd6932b9d4f0a16f2d0ab4bc8b00ad98a4f1fb
muneeebahmed15 <muneebahme999@gmail.com>
2024-06-10 15:21:14 +0500

    m

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ connectDB();
 //     credentials: true,
 //   })
 // );
-// app.use(cors({
-//       origin: 'https://camprescue.vercel.app//',
-//       credentials: true,
-//     }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(cors());
 
